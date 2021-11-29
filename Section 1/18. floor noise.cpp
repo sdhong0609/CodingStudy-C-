0644cc9e032afc117854c8a63837776a785abcf5
sdhong <78577085+sdhong0609@users.noreply.github.com>
2021-11-29 09:18:36 +0900

    fix : update Question 18

--- Section 1/18. floor noise.cpp
+++ Section 1/18. floor noise.cpp
@@ -1,18 +1,16 @@
 #include <iostream>
 using namespace std;
 
-int arr[101];
 int main() {
-    int n, m, i, cnt=0, max=0;
+    int n, m, a, i, cnt=0, max=0;
     cin >> n >> m;
     for (i=0; i<n; i++) {
-        cin >> arr[i];
-        if (arr[i]>m) cnt++;
-        else {
-            if (max < cnt) max = cnt;
-            cnt=0;
-        }
+        cin >> a;
+        if (m < a) cnt++;
+        else cnt=0;
+        if (max < cnt) max = cnt;
     }
-    cout << max << endl;
+    if (max == 0) cout << "-1" << endl;
+    else cout << max << endl;
     return 0;
 }
