ed66e1e83dcbe2c21aa3aa14520dc201eef8831d
sdhong <78577085+sdhong0609@users.noreply.github.com>
2021-12-02 08:51:19 +0900

    fix : update Question 19

--- Section 1/19. anger-causing person.cpp
+++ Section 1/19. anger-causing person.cpp
@@ -3,16 +3,17 @@ using namespace std;
 
 int a[101];
 int main() {
-    int n, i, j, cnt=0;
+    int n, i, max=0, cnt=0;
     cin >> n;
-    for (i=0; i<n; i++) {
+    for (i=1; i<=n; i++) {
         cin >> a[i];
     }
-    for (i=0; i<=n-2; i++) {
-        for (j=i+1; j<=n-1; j++) {
-            if (a[i]<a[j]) break;
+    max = a[n];
+    for (i=n-1; i>=1; i--) {
+        if (max < a[i]) {
+            max = a[i];
+            cnt++;
         }
-        if (j==n) cnt++;
     }
     cout << cnt << endl;
     return 0;
