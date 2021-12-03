30d6ad57b55eaab88c1e6f0f60da25a5c2c1c0bf
sdhong <78577085+sdhong0609@users.noreply.github.com>
2021-12-03 09:25:55 +0900

    fix : update Question 20

--- Section 1/20. rock paper scissors.cpp
+++ Section 1/20. rock paper scissors.cpp
@@ -13,8 +13,10 @@ int main() {
     }
     for (i=1; i<=n; i++) {
         if (a[i]==b[i]) cout << "D" << endl;
-        else if ((a[i]==3 && b[i]==1) || a[i]<b[i]) cout << "B" << endl;
-        else if ((a[i]==1 && b[i]==3) || a[i]>b[i]) cout << "A" << endl;
+        else if (a[i]==1 && b[i]==3) cout << "A" << endl;
+        else if (a[i]==2 && b[i]==1) cout << "A" << endl;
+        else if (a[i]==3 && b[i]==2) cout << "A" << endl;
+        else cout << "B" << endl;
     }
     return 0;
 }
